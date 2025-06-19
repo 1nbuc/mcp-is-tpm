dd93dd165e4b39bb7e0f6c53be42359d54b9398e
Noah Buchmann <noah.buchmann@contriboot.de>
2025-06-19 20:51:26 +0200

    Add custom b2bscenario client

---
+++ app/src/main/java/de/contriboot/mcptpm/api/B2BScenarioClientExtended.java
@@ -0,0 +1,62 @@
+package de.contriboot.mcptpm.api;
+
+import com.figaf.integration.common.entity.RequestContext;
+import com.figaf.integration.common.exception.ClientIntegrationException;
+import com.figaf.integration.common.factory.HttpClientsFactory;
+import com.figaf.integration.tpm.client.b2bscenario.B2BScenarioClient;
+import com.figaf.integration.tpm.entity.B2BScenarioMetadata;
+import com.figaf.integration.tpm.entity.TpmObjectMetadata;
+import com.figaf.integration.tpm.entity.trading.CreateTradingPartnerRequest;
+import com.figaf.integration.tpm.parser.B2BScenarioResponseParser;
+import com.figaf.integration.tpm.parser.TradingPartnerVerboseParser;
+import de.contriboot.mcptpm.api.entities.B2BScenarioEntity;
+import org.springframework.ai.tool.ToolCallback;
+import org.springframework.ai.tool.definition.ToolDefinition;
+import org.springframework.ai.tool.method.MethodToolCallback;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.*;
+
+import java.util.List;
+
+import static com.figaf.integration.tpm.utils.TpmUtils.PATH_FOR_TOKEN;
+import static java.lang.String.format;
+
+public class B2BScenarioClientExtended extends B2BScenarioClient {
+    public B2BScenarioClientExtended(HttpClientsFactory httpClientsFactory) {
+        super(httpClientsFactory);
+    }
+
+    public String getB2BScenariosForAgreementRaw(RequestContext requestContext, TpmObjectMetadata agreementMetadata) {
+        return executeGet(
+                requestContext,
+                format(B2B_SCENARIOS_RESOURCE, agreementMetadata.getObjectId()),
+                (response) -> response
+        );
+    }
+
+    @Bean
+    public MethodToolCallback updateB2BScenario(RequestContext requestContext, B2BScenarioEntity b2bScenarioEntity) {
+        executeMethod(
+                requestContext,
+                PATH_FOR_TOKEN,
+                format(B2B_SCENARIOS_RESOURCE, b2bScenarioEntity.getParentId()),
+                (url, token, restTemplateWrapper) -> {
+                    HttpHeaders httpHeaders = createHttpHeadersWithCSRFToken(token);
+                    httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+                    HttpEntity<B2BScenarioEntity> requestEntity = new HttpEntity<>(b2bScenarioEntity, httpHeaders);
+                    ResponseEntity<String> responseEntity = restTemplateWrapper.getRestTemplate().exchange(url, HttpMethod.PUT, requestEntity, String.class);
+                    if (!responseEntity.getStatusCode().is2xxSuccessful()) {
+                        throw new ClientIntegrationException(format(
+                                "Couldn't update b2bscenario. Code: %d, Message: %s",
+                                responseEntity.getStatusCode().value(),
+                                requestEntity.getBody())
+                        );
+                    }
+
+
+                    return "success";
+                });
+
+        return MethodToolCallback.builder().toolDefinition(ToolDefinition.builder().name("update-b2b-scenario").build()).build();
+    }
+}
