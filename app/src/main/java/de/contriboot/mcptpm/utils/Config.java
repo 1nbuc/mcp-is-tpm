2f1b37cea5bc0b87f22d9330c5b11af31da8e863
Noah Buchmann <noah.buchmann@contriboot.de>
2025-06-19 20:51:33 +0200

    Add config based on .env

--- app/src/main/java/de/contriboot/mcptpm/utils/Config.java
+++ app/src/main/java/de/contriboot/mcptpm/utils/Config.java
@@ -1,9 +1,49 @@
 package de.contriboot.mcptpm.utils;
 
-import com.figaf.integration.common.entity.RequestContext;
+import com.figaf.integration.common.entity.*;
+import io.github.cdimascio.dotenv.Dotenv;
 
 public class Config {
     public static RequestContext getRequestContextFromEnv() {
-        return null;
+        RequestContext requestContext = new RequestContext();
+
+        requestContext.setCloudPlatformType(CloudPlatformType.CLOUD_FOUNDRY);
+        requestContext.setAuthenticationType(AuthenticationType.BASIC);
+        requestContext.setPlatform(Platform.CPI);
+        requestContext.setWebApiAccessMode(WebApiAccessMode.SAP_UNIVERSAL_ID);
+        requestContext.setConnectionProperties(
+                new ConnectionProperties(
+                        Config.getEnvVar("CPI_URL") ,
+                        Config.getEnvVar("CPI_USER") ,
+                        Config.getEnvVar("CPI_PASSWORD")
+                ));
+        requestContext.setUniversalAuthAccountId(Config.getEnvVar("CPI_UNIVERSAL_AUTH_ACCOUNT_ID") );
+
+        return requestContext;
+    }
+
+    public static String getEnvVar(String key) {
+        Dotenv dotenv = Dotenv.load();
+        String result = System.getenv(key);
+
+        if (result == null || result.isEmpty()) {
+            result = dotenv.get(key);
+        }
+
+        if (result == null || result.isEmpty()) {
+            throw new RuntimeException("Environment variable '" + key + "' is not set.");
+        }
+
+        return result;
+    }
+
+    public static String getEnvPath() {
+        String binPath = Config.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+        String[] split = binPath.split("/");
+        String envPath = "";
+        for (int i = 0; i < split.length - 1; i++) {
+            envPath += split[i] + "/";
+        }
+        return envPath + "env";
     }
 }
